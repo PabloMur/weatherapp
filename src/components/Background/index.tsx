f432b1b1b5beaab0fe05b514d329ed3ee4aba173
PabloMur <polillomurillo@gmail.com>
2023-05-17 23:36:43 -0300

    estatus

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,10 +1,40 @@
 import css from "./styles.module.css";
 
-const Morning = () => {};
-const Noon = () => {};
-const Afternoon = () => {};
-const Evening = () => {};
-const Night = () => {};
+const Morning = ({ children }: any) => {
+  return (
+    <>
+      <div className={css.night}>{children}</div>
+    </>
+  );
+};
+const Noon = ({ children }: any) => {
+  return (
+    <>
+      <div className={css.night}>{children}</div>
+    </>
+  );
+};
+const Afternoon = ({ children }: any) => {
+  return (
+    <>
+      <div className={css.night}>{children}</div>
+    </>
+  );
+};
+const Evening = ({ children }: any) => {
+  return (
+    <>
+      <div className={css.night}>{children}</div>
+    </>
+  );
+};
+const Night = ({ children }: any) => {
+  return (
+    <>
+      <div className={css.night}>{children}</div>
+    </>
+  );
+};
 const Midnight = ({ children }: any) => {
   return (
     <>
