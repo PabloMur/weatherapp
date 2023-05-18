579562c6b39b24093a4528fdccb140f193d37186
PabloMur <polillomurillo@gmail.com>
2023-05-17 23:49:10 -0300

    a ver si ahora anda esto

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,47 +1,47 @@
 import css from "./styles.module.css";
 
-const Morning = ({ children }: any) => {
-  return (
-    <>
-      <div className={css.night}>{children}</div>
-    </>
-  );
-};
-const Noon = ({ children }: any) => {
-  return (
-    <>
-      <div className={css.night}>{children}</div>
-    </>
-  );
-};
-const Afternoon = ({ children }: any) => {
-  return (
-    <>
-      <div className={css.night}>{children}</div>
-    </>
-  );
-};
-const Evening = ({ children }: any) => {
-  return (
-    <>
-      <div className={css.night}>{children}</div>
-    </>
-  );
-};
-const Night = ({ children }: any) => {
-  return (
-    <>
-      <div className={css.night}>{children}</div>
-    </>
-  );
-};
-const Midnight = ({ children }: any) => {
-  return (
-    <>
-      <div className={css.night}>{children}</div>
-    </>
-  );
-};
+// const Morning = ({ children }: any) => {
+//   return (
+//     <>
+//       <div className={css.night}>{children}</div>
+//     </>
+//   );
+// };
+// const Noon = ({ children }: any) => {
+//   return (
+//     <>
+//       <div className={css.night}>{children}</div>
+//     </>
+//   );
+// };
+// const Afternoon = ({ children }: any) => {
+//   return (
+//     <>
+//       <div className={css.night}>{children}</div>
+//     </>
+//   );
+// };
+// const Evening = ({ children }: any) => {
+//   return (
+//     <>
+//       <div className={css.night}>{children}</div>
+//     </>
+//   );
+// };
+// const Night = ({ children }: any) => {
+//   return (
+//     <>
+//       <div className={css.night}>{children}</div>
+//     </>
+//   );
+// };
+// const Midnight = ({ children }: any) => {
+//   return (
+//     <>
+//       <div className={css.night}>{children}</div>
+//     </>
+//   );
+// };
 export const Background = ({ children }: any) => {
   return (
     <>
