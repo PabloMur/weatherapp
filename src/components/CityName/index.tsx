4a63c0e38dde3b8cbc31bd684220fd688e138bd9
PabloMur <polillomurillo@gmail.com>
2023-05-26 13:06:47 -0300

    arregle mobil y rompi lo otro jajaja

--- src/components/CityName/index.tsx
+++ src/components/CityName/index.tsx
@@ -1,10 +1,14 @@
 import css from "./styles.module.css";
 import icon from "../../assets/vacations.png";
+import lion from "../../assets/sea-lion.png";
 export const CityName = ({ cityname }: any) => {
   return (
     <>
       <div className={css.city}>
         <h2 className={css.cityname}>{cityname}</h2>
+      </div>
+      <div className={css.imgCont}>
+        <img className={css.img} src={lion} alt="sea lion" />
       </div>
       <div className={css.imgCont}>
         <img className={css.img} src={icon} alt="beach" />
