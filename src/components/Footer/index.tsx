3973af810c934cf26b69ba7df61e04af5fcbe077
PabloMur <polillomurillo@gmail.com>
2023-05-30 12:57:00 -0300

    footer mobile

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 import css from "./styles.module.css";
 export const Footer = () => {
   return (
-    <>
-      <div className={css.root}>
+    <div className={css.root}>
+      <p className={css["p"]}>
         Copyright © 2023 Pablo Murillo | Todos los derechos reservados.
-      </div>
-    </>
+      </p>
+    </div>
   );
 };
