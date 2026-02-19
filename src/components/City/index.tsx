b909fbd68e6e8b883103ed3992c399e3c3ad034a
PabloMur <polillomurillo@gmail.com>
2026-02-19 18:01:41 -0300

    asd

--- src/components/City/index.tsx
+++ src/components/City/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import css from "./styles.module.css";
+import { Spinner } from "../ui/Spinner";
 
 const City: React.FC<{ name: string; temp: any }> = ({ name, temp }) => (
   <div className={css.root}>
     <p>{name}</p>
-    <p className={css.temp}>{temp}</p>
+    {temp ? <p className={css.temp}>{temp}</p> : <Spinner></Spinner>}
   </div>
 );
 
---
+++ src/components/ui/Spinner/index.tsx
@@ -0,0 +1,4 @@
+import css from "./styles.module.css";
+export const Spinner = () => {
+  return <p className={css.spinner}>Cargando</p>;
+};
